9dd2ee3c28a1a4fbb50511c22b30c4c90354d855
Uthmanbello <uthmanbello88@gmail.com>
2023-09-17 22:41:02 +0100

    Setup choose ride panel

---
+++ scripts/rideOptions.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const chooseRide = document.getElementById("choose-ride");
+    const rideOptions = document.getElementById("ride-options");
+  
+    chooseRide.addEventListener("click", function () {
+      rideOptions.classList.toggle("show");
+      chooseRide.classList.toggle("active");
+    });
+  });
+  
